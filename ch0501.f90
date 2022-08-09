aad107e33e3da1d4b2b31bcc46d035a6423f9648
Sopanat Suradach <sopanus2014@gmail.com>
2022-08-09 13:16:37 +0700

    feat : added ch0501 and ch0502

---
+++ ch0501.f90
@@ -0,0 +1,24 @@
+ program ch0501
+    implicit none
+  !
+  ! Example of a Fortran program
+  ! to calculate net pay
+  ! given an employee’s gross pay
+  !
+    real :: gross_wage, net_wage, tax
+    real :: tax_rate = 0.25
+    integer :: personal_allowance = 10000
+    character (len=60) :: their_name
+    print *, 'Input employees name :'
+    read *, their_name
+    print *, 'Input Gross wage :' 
+    read *, gross_wage
+
+    tax = (gross_wage-personal_allowance)*tax_rate
+    net_wage = gross_wage - tax
+    print *, '----- Result -------'
+    print *, 'Employee: ', their_name
+    print *, 'Gross Pay: ', gross_wage
+    print *, 'Tax: ', tax
+    print *, 'Net Pay:', net_wage
+  end program ch0501
---
+++ ch0502.f90
@@ -0,0 +1,14 @@
+! ch0502
+program ch0502
+    implicit none
+    real :: a, b, c
+    integer :: i
+    a = 1.5
+    b = 2.0
+    c = a/b
+    i = a/b
+    ! print results   
+    print *, a, b
+    print *, c
+    print *, i
+  end program ch0502
